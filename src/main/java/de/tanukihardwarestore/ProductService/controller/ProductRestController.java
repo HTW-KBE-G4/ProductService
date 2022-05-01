e8d5c4c7264246f4a28fa5db03d1819f28a93f7a
profanta3 <karl@neukoelln-berlin.de>
2022-05-01 20:25:58 +0200

    THS-93 added simple RESTController and Component data manager

--- src/main/java/de/tanukihardwarestore/ProductService/controller/ProductRestController.java
+++ src/main/java/de/tanukihardwarestore/ProductService/controller/ProductRestController.java
@@ -1,4 +1,37 @@
 package de.tanukihardwarestore.ProductService.controller;
 
+import de.tanukihardwarestore.ProductService.warehouse.ComponentManager;
+import de.tanukihardwarestore.ProductService.warehouse.PCComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductRestController {
+
+    private static final String URL_PATH = "http://localhost:3002/component";
+
+    @Autowired
+    private ComponentManager componentManager;
+
+    @GetMapping("")
+    public String getWelcomeMessage() {
+        return "<h1>Welcome to the Product Service!</h1>" +
+                "<br>" +
+                "<a href='http://localhost:4200/component'>compoennts</a>" +
+                "<br>" +
+                "<a href='http://localhost:4200/product'>products</a>" +
+                "<br>" +
+                "<a href='http://localhost:4200/h2-console'>H2-Console</a>";
+    }
+
+    @GetMapping("component")
+    public List<PCComponent> getcomponents() {
+        if (componentManager.fetchData(URL_PATH) == false) {
+            return null;
+        }
+        return componentManager.getAll().stream().toList();
+    }
 }
--- src/main/java/de/tanukihardwarestore/ProductService/warehouse/ComponentManager.java
+++ src/main/java/de/tanukihardwarestore/ProductService/warehouse/ComponentManager.java
@@ -1,2 +1,26 @@
-package de.tanukihardwarestore.ProductService.warehouse;public interface ComponentManager {
+package de.tanukihardwarestore.ProductService.warehouse;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface ComponentManager {
+
+    /**
+     * Get all Components from backend
+     * @return
+     */
+    Collection<PCComponent> getAll();
+
+    /**
+     * Get specific Component by api call
+     * @param id id of the compoennt
+     * @return
+     */
+    PCComponent getByID(Long id);
+
+    /**
+     * Init connection and fetch the data from Warehouse-Microservice
+     * @return
+     */
+    boolean fetchData(String url);
 }
