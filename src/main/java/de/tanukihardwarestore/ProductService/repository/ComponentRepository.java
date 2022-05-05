472c1988e10da82f008d0cf7dbab01b71f3df5ee
profanta3 <karl@neukoelln-berlin.de>
2022-05-05 23:39:20 +0200

    THS-107 update entities and their repositories

--- src/main/java/de/tanukihardwarestore/ProductService/repository/ComponentRepository.java
+++ src/main/java/de/tanukihardwarestore/ProductService/repository/ComponentRepository.java
@@ -1,6 +1,6 @@
 package de.tanukihardwarestore.ProductService.repository;
 
-import de.tanukihardwarestore.ProductService.warehouse.PCComponent;
+import de.tanukihardwarestore.ProductService.model.PCComponent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComponentRepository extends JpaRepository<PCComponent, Long> {
